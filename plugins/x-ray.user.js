8214959d17fef26efe2419174e6e4d5319280c0d
Vladimir Belozyorov <vladimir.belozyorov@gmail.com>
2015-06-01 23:59:32 +0300

    zoom level selector

--- plugins/x-ray.user.js
+++ plugins/x-ray.user.js
@@ -2,11 +2,11 @@
 // @id             iitc-plugin-x-ray
 // @name           IITC Plugin: X-Ray
 // @category       Portals
-// @version        0.1.0.@@DATETIMEVERSION@@
+// @version        1.0.0.@@DATETIMEVERSION@@
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      @@UPDATEURL@@
 // @downloadURL    @@DOWNLOADURL@@
-// @description    [@@BUILDNAME@@-@@BUILDDATE@@] Draw all portals for current view. May perform MANY requests to Intel site for large scales
+// @description    [@@BUILDNAME@@-@@BUILDDATE@@] Draw "specified or higher" portals for current view. May perform MANY requests to Intel site for large scales
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
@@ -25,13 +25,26 @@ window.plugin.xRay = function () {};
 
 window.plugin.xRay.originalFunc = undefined;
 window.plugin.xRay.xRayFunc = function (zoom) {
-    return 17;
+    return $('#xray-level').val();
 };
 
 window.plugin.xRay.setup = function () {
 
     window.plugin.xRay.layer = L.layerGroup([]);
-    window.addLayerGroup('Show ALL portals', plugin.xRay.layer, false);
+    window.addLayerGroup('Force <select id="xray-level">\
+        <option value="17">0+ portals</option>\
+        <option value="15">1+ portals</option>\
+        <option value="13">2+ portals</option>\
+        <option value="12">3+ portals</option>\
+        <option value="10">5+ portals</option>\
+        <option value="9">6+ portals</option>\
+        <option value="7">7+ portals</option>\
+        <option value="4">8+ portals</option>\
+    </select>', plugin.xRay.layer, false);
+
+    $('.leaflet-control-layers-list').on('change', '#xray-level', function(){
+        $(this).closest('label').find('input[type=checkbox]').prop('checked', true);
+    });
 
     window.map.on('layeradd', function (e) {
         if (e.layer === window.plugin.xRay.layer) {
